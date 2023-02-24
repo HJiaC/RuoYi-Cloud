40c1175b22a66ec2939a4b8e6fa26d312f343570
can <huangxj_tzc@163.com>
2023-02-24 17:15:38 +0800

    1、完成调度中心界面及接口

---
+++ ruoyi-ui/src/views/system/map/index.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+      <!-- <h1>志愿者调度平台首页</h1> -->
+      <MapContainer />
+    </div>
+      
+</template>
+
+<script>
+//导入高德地图组件
+import MapContainer from '@/views/tool/map/MapContainer'
+export default {
+  name: "Map",
+  components:{MapContainer},
+  data() {
+    return {
+      // 版本号
+      version: "3.6.2",
+    };
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+.home {
+  blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px solid #eee;
+  }
+  hr {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+  .col-item {
+    margin-bottom: 20px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 13px;
+  color: #676a6c;
+  overflow-x: hidden;
+
+  ul {
+    list-style-type: none;
+  }
+
+  h4 {
+    margin-top: 0px;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-size: 26px;
+    font-weight: 100;
+  }
+
+  p {
+    margin-top: 10px;
+
+    b {
+      font-weight: 700;
+    }
+  }
+
+  .update-log {
+    ol {
+      display: block;
+      list-style-type: decimal;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0;
+      margin-inline-end: 0;
+      padding-inline-start: 40px;
+    }
+  }
+}
+</style>
+
